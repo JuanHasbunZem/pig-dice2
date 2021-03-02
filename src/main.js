a9fe1577d98cc892513f6957952c4602d76bd666
JuanHasbunZem <zemenarwow@gmail.com>
2021-03-01 20:55:54 -0800

    suffer from npm run build errors
    Co-authored-by: marnionrails <marnionrails@users.noreply.github.com>
    Co-authored-by: shanole <shannonleehj@gmail.com>

---
+++ src/main.js
@@ -0,0 +1,63 @@
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
+import Player from './../src/player.js';
+import {playerSwitch, isWinner, showResults} from './../src/interface.js';
+
+let playerOne = new Player(1);
+let playerTwo = new Player(2);
+playerTwo.turn = false;
+
+$(document).ready(function() {
+  $(".roll").click(function() {
+    let id = $(this).attr('id');
+    let player;
+    let opponent;
+    if (id === "1") {
+      player = playerOne;
+      opponent = playerTwo;    
+    }
+    else if (id === "2") {
+      player = playerTwo;
+      opponent = playerOne;
+    }
+    player.roll();
+    showResults(id,player);
+    playerSwitch(player,opponent);
+    if (player.roll() === "no") {
+      alert("Please wait your turn!");
+    }
+    else {
+      if (isWinner(player)) {
+        // Declare Player One as the winner and stop the game!
+      }
+    }
+  });
+
+  $(".hold").click(function() {
+    let id = $(this).attr('id');
+    let player;
+    let opponent;
+    if (id === "1") {
+      player = playerOne;
+      opponent = playerTwo;    
+    }
+    else if (id === "2") {
+      player = playerTwo;
+      opponent = playerOne;
+    }
+    player.hold();
+    showResults(id,player);
+    playerSwitch(player,opponent);
+    if (player.hold() === "no") {
+      alert("Please wait your turn!");
+    }
+    else {
+      if (isWinner(player)) {
+        // Declare Player One as the winner and stop the game!
+      }
+    }
+  });
+});
+
